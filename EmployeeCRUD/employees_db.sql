2473e892b9151f59c4c44358cd9105392a23c9d5
JOLSANA JAIMON <71826232+jolsanajaimon@users.noreply.github.com>
2024-06-26 22:16:52 +0530

    Add files via upload

---
+++ EmployeeCRUD/employees_db.sql
@@ -0,0 +1,6 @@
+create database employees_db;
+use employees_db;
+CREATE TABLE employee_table(employee_id INT AUTO_INCREMENT PRIMARY KEY,employee_name VARCHAR(50),employee_salary INT);
+show tables;
+describe employee_table;
+select * from employee_table;
